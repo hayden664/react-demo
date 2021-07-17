efd0dd759125ec9d1770274375e4a5aef9031c20
Hayden Ng <hayden@enterpryze.com>
2021-07-17 23:17:19 +0800

    Removed unused stuff

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 import './index.css';
 import List from './components/list';
-import Sort from './components/sort';
-
-
 
 class App extends Component {
     render() { 
         return (
             <React.Fragment>
                 <main className="container-fluid">
-                    {/* <Sort /> */}
                     <List />
                 </main>
             </React.Fragment>
--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
-import List from './components/list';
-import Sort from './components/sort';
 import './index.css';
 
 
